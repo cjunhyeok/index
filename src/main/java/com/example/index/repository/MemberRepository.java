df9b8b94252955fd27f18388d7d51a5d958442ed
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-03 22:19:00 +0900

    Feat: username 으로 조회, nickname 으로 조회 쿼리 개발

--- src/main/java/com/example/index/repository/MemberRepository.java
+++ src/main/java/com/example/index/repository/MemberRepository.java
@@ -4,4 +4,7 @@ import com.example.index.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByUsername(String username);
+    Member findByNickname(String nickname);
 }
